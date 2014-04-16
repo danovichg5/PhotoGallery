db76a5f8ad042b4e22b70396df8275413561539a
Gary Danovich <danovichg05@mycu.concord.edu>
2014-04-15 23:30:50 -0400

    Chapter 31 done

--- src/com/bignerdranch/android/photogallery/GalleryItem.java
+++ src/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -4,6 +4,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     public String getCaption() {
         return mCaption;
@@ -24,6 +25,18 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+    
+    public String getOwner() {
+    	return mOwner;
+    }
+    
+    public void setOwner(String owner) {
+    	mOwner = owner;
+    }
+    
+    public String getPhotoPageUrl() {
+    	return "http://www.flickr.com/photos/" + mOwner + "/" + mId;
     }
 
     public String toString() {
